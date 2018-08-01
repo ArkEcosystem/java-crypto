36d303e27982d2ec5db9568d54e1f25b5e748997
Brian Faust <envoyer@pm.me>
2018-08-01 06:40:05 +0300

    test: rename test variables
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/test/groovy/org/arkecosystem/crypto/identities/PrivateKeyTest.groovy
+++ src/test/groovy/org/arkecosystem/crypto/identities/PrivateKeyTest.groovy
@@ -4,8 +4,8 @@ import org.arkecosystem.crypto.identities.*
 class PrivateKeyTest extends Specification {
     def "fromPassphrase"() {
         when:
-            def address = PrivateKey.fromPassphrase("this is a top secret passphrase").getPrivateKeyAsHex()
+            def privateKey = PrivateKey.fromPassphrase("this is a top secret passphrase").getPrivateKeyAsHex()
         then:
-            address == 'd8839c2432bfd0a67ef10a804ba991eabba19f154a3d707917681d45822a5712'
+            privateKey == 'd8839c2432bfd0a67ef10a804ba991eabba19f154a3d707917681d45822a5712'
     }
 }

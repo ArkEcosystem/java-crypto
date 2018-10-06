47d34517d8aa36a4c24f9c3fea8bf35af6c653c2
supaiku <supaiku@protonmail.com>
2018-10-06 03:48:18 +0200

    port: VoteTest deserializer

--- src/test/java/org/arkecosystem/crypto/transactions/deserializers/VoteTest.java
+++ src/test/java/org/arkecosystem/crypto/transactions/deserializers/VoteTest.java
@@ -1,43 +1,57 @@
-package org.arkecosystem.crypto.transactions.deserializers
+package org.arkecosystem.crypto.transactions.deserializers;
 
+import com.google.gson.internal.LinkedTreeMap;
+import org.arkecosystem.crypto.transactions.Deserializer;
+import org.arkecosystem.crypto.transactions.FixtureLoader;
+import org.arkecosystem.crypto.transactions.Transaction;
+import org.junit.jupiter.api.Test;
 
-import spock.lang.Specification
-import org.arkecosystem.crypto.transactions.*
+import java.util.HashMap;
+import java.util.List;
 
-class VoteTest extends Specification {
-    def "passphrase"() {
-        setup:
-            Object fixture = FixtureLoader.load(getClass(), "transactions/vote/passphrase")
-        when:
-            def actual = new Deserializer().deserialize(fixture.serialized)
-        then:
-            actual.type == fixture.data.type
-            actual.amount == fixture.data.amount
-            actual.fee == fixture.data.fee
-            actual.recipientId == fixture.data.recipientId
-            actual.timestamp == fixture.data.timestamp
-            actual.senderPublicKey == fixture.data.senderPublicKey
-            actual.signature == fixture.data.signature
-            actual.signSignature == fixture.data.signSignature
-            actual.asset.votes == fixture.data.asset.votes
-            actual.id == fixture.data.id
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VoteTest {
+
+    @Test
+    void passphrase() {
+        HashMap<String, Object> fixture = FixtureLoader.load("transactions/vote/passphrase");
+        LinkedTreeMap<String, Object> data = (LinkedTreeMap<String, Object>) fixture.get("data");
+
+        Transaction actual = new Deserializer().deserialize(fixture.get("serialized").toString());
+        assertEquals(((Double)data.get("type")).intValue(), actual.type.getValue());
+        assertEquals(((Double)data.get("amount")).longValue(), actual.amount);
+        assertEquals(((Double)data.get("fee")).longValue(), actual.fee);
+        assertEquals(data.get("recipientId").toString(), actual.recipientId);
+        assertEquals(((Double)data.get("timestamp")).intValue(), actual.timestamp);
+        assertEquals(data.get("senderPublicKey").toString(), actual.senderPublicKey);
+        assertEquals(data.get("signature").toString(), actual.signature);
+
+        LinkedTreeMap<String, Object> asset = (LinkedTreeMap<String, Object>) data.get("asset");
+        assertEquals((List<String>)(asset.get("votes")), actual.asset.votes);
+
+        assertEquals(data.get("id").toString(), actual.id);
     }
 
-    def "second-passphrase"() {
-        setup:
-            Object fixture = FixtureLoader.load(getClass(), "transactions/vote/second-passphrase")
-        when:
-            def actual = new Deserializer().deserialize(fixture.serialized)
-        then:
-            actual.type == fixture.data.type
-            actual.amount == fixture.data.amount
-            actual.fee == fixture.data.fee
-            actual.recipientId == fixture.data.recipientId
-            actual.timestamp == fixture.data.timestamp
-            actual.senderPublicKey == fixture.data.senderPublicKey
-            actual.signature == fixture.data.signature
-            actual.signSignature == fixture.data.signSignature
-            actual.asset.votes == fixture.data.asset.votes
-            actual.id == fixture.data.id
+    @Test
+    void secondPassphrase() {
+        HashMap<String, Object> fixture = FixtureLoader.load("transactions/vote/second-passphrase");
+        LinkedTreeMap<String, Object> data = (LinkedTreeMap<String, Object>) fixture.get("data");
+
+        Transaction actual = new Deserializer().deserialize(fixture.get("serialized").toString());
+        assertEquals(((Double)data.get("type")).intValue(), actual.type.getValue());
+        assertEquals(((Double)data.get("amount")).longValue(), actual.amount);
+        assertEquals(((Double)data.get("fee")).longValue(), actual.fee);
+        assertEquals(data.get("recipientId").toString(), actual.recipientId);
+        assertEquals(((Double)data.get("timestamp")).intValue(), actual.timestamp);
+        assertEquals(data.get("senderPublicKey").toString(), actual.senderPublicKey);
+        assertEquals(data.get("signature").toString(), actual.signature);
+        assertEquals(data.get("signSignature").toString(), actual.signSignature);
+
+        LinkedTreeMap<String, Object> asset = (LinkedTreeMap<String, Object>) data.get("asset");
+        assertEquals((List<String>)(asset.get("votes")), actual.asset.votes);
+
+        assertEquals(data.get("id").toString(), actual.id);
     }
+
 }

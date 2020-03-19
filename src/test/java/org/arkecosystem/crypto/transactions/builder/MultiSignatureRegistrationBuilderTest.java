c18df7686ec1796e4fd9a6375e61f14314fb2ccf
Kristjan Kosic - Chris <chris@ark.io>
2020-03-19 11:18:10 +0100

    style: spotless on tests

--- src/test/java/org/arkecosystem/crypto/transactions/builder/MultiSignatureRegistrationBuilderTest.java
+++ src/test/java/org/arkecosystem/crypto/transactions/builder/MultiSignatureRegistrationBuilderTest.java
@@ -3,20 +3,20 @@ package org.arkecosystem.crypto.transactions.builder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
+import org.junit.jupiter.api.Test;
 
 public class MultiSignatureRegistrationBuilderTest {
 
     @Test
     void build() {
-        MultiSignatureRegistrationBuilder actual = new MultiSignatureRegistrationBuilder()
-            .keysgroup(Arrays.asList("keysgroup"))
-            .lifetime(1)
-            .lifetime((byte)1)
-            .min(1)
-            .min((byte)1);
+        MultiSignatureRegistrationBuilder actual =
+                new MultiSignatureRegistrationBuilder()
+                        .keysgroup(Arrays.asList("keysgroup"))
+                        .lifetime(1)
+                        .lifetime((byte) 1)
+                        .min(1)
+                        .min((byte) 1);
 
         Throwable exception =
                 assertThrows(UnsupportedOperationException.class, () -> actual.sign("passphrase"));

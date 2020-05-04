5ae197dccb93129e747cd4305f16b872c7c0df8d
KovacZan <39158639+KovacZan@users.noreply.github.com>
2020-05-04 10:22:26 +0200

    feat: added option to add custom deserializer classes to Deserializer (#141)
    * feat: added option to add custom deserializer classes to Deserializer
    
    * fix: remove commented code
    
    * chore: apply spotless
    
    * fix: tests

---
+++ src/test/java/org/arkecosystem/crypto/transactions/deserializers/DeserializerTest.java
@@ -0,0 +1,86 @@
+package org.arkecosystem.crypto.transactions.deserializers;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.google.gson.internal.LinkedTreeMap;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import org.arkecosystem.crypto.transactions.Deserializer;
+import org.arkecosystem.crypto.transactions.FixtureLoader;
+import org.arkecosystem.crypto.transactions.types.Transaction;
+import org.junit.jupiter.api.Test;
+
+class DeserializerTest {
+    @Test
+    void checkNewTransactionType() {
+        LinkedTreeMap<String, Object> fixture =
+                FixtureLoader.load("transactions/v2-ecdsa/transfer-sign");
+
+        Deserializer deserializer = new Deserializer(fixture.get("serialized").toString());
+        deserializer.setNewTransactionType(
+                new Transaction() {
+                    @Override
+                    public byte[] serialize() {
+                        return new byte[0];
+                    }
+
+                    @Override
+                    public void deserialize(ByteBuffer buffer) {}
+
+                    @Override
+                    public int getTransactionType() {
+                        return 1;
+                    }
+
+                    @Override
+                    public int getTransactionTypeGroup() {
+                        return 2;
+                    }
+
+                    @Override
+                    public HashMap<String, Object> assetToHashMap() {
+                        return null;
+                    }
+                });
+
+        assertTrue(deserializer.hasTransactionType(2, 1));
+        assertFalse(deserializer.hasTransactionType(2, 2));
+        assertFalse(deserializer.hasTransactionType(3, 1));
+
+    }
+
+    @Test
+    void checkNewTransactionToCoreGroup() {
+        LinkedTreeMap<String, Object> fixture =
+            FixtureLoader.load("transactions/v2-ecdsa/transfer-sign");
+
+        Deserializer deserializer = new Deserializer(fixture.get("serialized").toString());
+        deserializer.setNewTransactionType(
+            new Transaction() {
+                @Override
+                public byte[] serialize() {
+                    return new byte[0];
+                }
+
+                @Override
+                public void deserialize(ByteBuffer buffer) {}
+
+                @Override
+                public int getTransactionType() {
+                    return 11;
+                }
+
+                @Override
+                public int getTransactionTypeGroup() {
+                    return 1;
+                }
+
+                @Override
+                public HashMap<String, Object> assetToHashMap() {
+                    return null;
+                }
+            });
+        assertTrue(deserializer.hasTransactionType(1, 11));
+    }
+}

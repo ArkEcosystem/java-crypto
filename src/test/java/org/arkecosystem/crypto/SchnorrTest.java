d03b110df4a4c418c433ee0848a19140816df321
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2020-11-16 11:38:31 -0300

    Cleanup

--- src/test/java/org/arkecosystem/crypto/SchnorrTest.java
+++ src/test/java/org/arkecosystem/crypto/SchnorrTest.java
@@ -11,17 +11,17 @@ class SchnorrTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/utils/test-vectors.csv", numLinesToSkip = 1)
-    void name(String index, String seckey, String publickey, String message, String sig, boolean result, String comment) {
+    void name(String secretKey, String publickey, String message, String signature, boolean expected, String comment) {
 
         byte[] pubkey = Schnorr.hexStringToByteArray(publickey);
         byte[] msg = Schnorr.hexStringToByteArray(message);
 
-        if (seckey != null) {
-            BigInteger seckeyNum = new BigInteger(seckey, 16);
+        if (secretKey != null) {
+            BigInteger seckeyNum = new BigInteger(secretKey, 16);
             String sig_actual = Schnorr.bytesToHex(Schnorr.schnorr_sign(msg, seckeyNum));
-            assertEquals(sig, sig_actual);
+            assertEquals(signature, sig_actual);
         }
-        boolean result_actual = Schnorr.schnorr_verify(msg, pubkey, Schnorr.hexStringToByteArray(sig));
-        assertEquals(result, result_actual, comment);
+        boolean result_actual = Schnorr.schnorr_verify(msg, pubkey, Schnorr.hexStringToByteArray(signature));
+        assertEquals(expected, result_actual, comment);
     }
 }

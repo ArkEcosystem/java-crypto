649babfc17d90321cb8350558c931fcf581dd565
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-08-05 12:07:51 -0600

    vote builder tests & adjustments

--- src/main/java/org/arkecosystem/crypto/transactions/builder/VoteBuilder.java
+++ src/main/java/org/arkecosystem/crypto/transactions/builder/VoteBuilder.java
@@ -22,6 +22,16 @@ public class VoteBuilder extends AbstractTransactionBuilder<VoteBuilder> {
         this.transaction.asset.votes.add(vote);
         return this;
     }
+    
+    public VoteBuilder addUnvotes(List unvotes) {
+        this.transaction.asset.unvotes = unvotes;
+        return this;
+    }
+
+    public VoteBuilder addUnvote(String unvote) {
+        this.transaction.asset.unvotes.add(unvote);
+        return this;
+    }
 
     public VoteBuilder sign(String passphrase) {
         this.transaction.recipientId = Address.fromPassphrase(passphrase);

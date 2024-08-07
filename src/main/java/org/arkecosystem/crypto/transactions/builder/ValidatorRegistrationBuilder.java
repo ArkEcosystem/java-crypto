b590d2a88f6c43ec7b6148847153d321944b14f6
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-08-07 12:29:22 -0600

    basic public key validation

--- src/main/java/org/arkecosystem/crypto/transactions/builder/ValidatorRegistrationBuilder.java
+++ src/main/java/org/arkecosystem/crypto/transactions/builder/ValidatorRegistrationBuilder.java
@@ -6,14 +6,16 @@ import org.arkecosystem.crypto.transactions.types.ValidatorRegistration;
 
 public class ValidatorRegistrationBuilder
         extends AbstractTransactionBuilder<ValidatorRegistrationBuilder> {
+
     public ValidatorRegistrationBuilder() {
         super();
         this.transaction.fee = Fees.VALIDATOR_REGISTRATION.getValue();
     }
 
     public ValidatorRegistrationBuilder publicKeyAsset(String publicKey) {
-        this.transaction.asset.validatorPublicKey = publicKey;
+        validateBlsPublicKey(publicKey);
 
+        this.transaction.asset.validatorPublicKey = publicKey;
         return this;
     }
 
@@ -25,5 +27,15 @@ public class ValidatorRegistrationBuilder
     @Override
     public ValidatorRegistrationBuilder instance() {
         return this;
+    }
+
+    private void validateBlsPublicKey(String publicKeyHex) {
+        if (publicKeyHex.length() != 96) {
+            throw new IllegalArgumentException("Invalid BLS public key length");
+        }
+
+        if (!publicKeyHex.matches("^[0-9A-Fa-f]+$")) {
+            throw new IllegalArgumentException("Invalid BLS public key");
+        }
     }
 }
--- src/test/java/org/arkecosystem/crypto/transactions/builder/ValidatorRegistrationBuilderTest.java
+++ src/test/java/org/arkecosystem/crypto/transactions/builder/ValidatorRegistrationBuilderTest.java
@@ -27,4 +27,42 @@ class ValidatorRegistrationBuilderTest {
 
         assertTrue(actual.verify());
     }
+
+    @Test
+    void testValidatorRegistrationTransactionWithInvalidBlsPublicKey() {
+        Exception exception =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> {
+                            new ValidatorRegistrationBuilder()
+                                    .publicKeyAsset(
+                                            "a5fea88b9aab3f0b122e5a7e1b07917e62a63ea59103d0a0715ecded3c41685af88f0a9606309b148b3b50f51a2edddg")
+                                    .nonce(3)
+                                    .sign("this is a top secret passphrase");
+                        });
+
+        String expectedMessage = "Invalid BLS public key";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void testValidatorRegistrationTransactionWithInvalidBlsPublicKeyByLength() {
+        Exception exception =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> {
+                            new ValidatorRegistrationBuilder()
+                                    .publicKeyAsset(
+                                            "023efc1da7f315f3c533a4080e491f32cd4219731cef008976c3876539e1f192d3")
+                                    .nonce(3)
+                                    .sign("this is a top secret passphrase");
+                        });
+
+        String expectedMessage = "Invalid BLS public key";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 }

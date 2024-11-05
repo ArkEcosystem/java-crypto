49562b0c49bcbabf2a72ecd13f27c3560a3eee57
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-11-05 07:52:45 -0600

    wip

--- src/main/java/org/arkecosystem/crypto/transactions/builder/AbstractTransactionBuilder.java
+++ src/main/java/org/arkecosystem/crypto/transactions/builder/AbstractTransactionBuilder.java
@@ -7,12 +7,15 @@ import org.arkecosystem.crypto.enums.TransactionTypeGroup;
 import org.arkecosystem.crypto.transactions.TransactionAsset;
 import org.arkecosystem.crypto.transactions.types.AbstractTransaction;
 
-public abstract class AbstractTransactionBuilder<
-        TBuilder extends AbstractTransactionBuilder<TBuilder>> {
+public abstract class AbstractTransactionBuilder<TBuilder extends AbstractTransactionBuilder<TBuilder>> {
     public final AbstractTransaction transaction;
 
     public AbstractTransactionBuilder() {
         this.transaction = getTransactionInstance();
+        initializeTransactionDefaults();
+    }
+
+    private void initializeTransactionDefaults() {
         this.transaction.type = CoreTransactionTypes.EVM_CALL.getValue();
         this.transaction.typeGroup = TransactionTypeGroup.CORE.getValue();
         this.transaction.amount = 0;
@@ -23,12 +26,22 @@ public abstract class AbstractTransactionBuilder<
         this.transaction.nonce = 1;
 
         this.transaction.asset = new TransactionAsset();
-    this.transaction.asset.evmCall.gasLimit = 1000000; // Default gas limit
-    this.transaction.asset.evmCall.payload = ""; 
+        this.transaction.asset.evmCall.gasLimit = 1000000;
+        this.transaction.asset.evmCall.payload = "";
     }
 
-    public TBuilder version(int version) {
-        this.transaction.version = version;
+    public TBuilder gasLimit(int gasLimit) {
+        this.transaction.asset.evmCall.gasLimit = gasLimit;
+        return this.instance();
+    }
+
+    public TBuilder recipient(String recipientId) {
+        this.transaction.recipientId = recipientId;
+        return this.instance();
+    }
+
+    public TBuilder fee(long fee) {
+        this.transaction.fee = fee;
         return this.instance();
     }
 
@@ -42,35 +55,34 @@ public abstract class AbstractTransactionBuilder<
         return this.instance();
     }
 
-    public TBuilder fee(long fee) {
-        this.transaction.fee = fee;
-        return this.instance();
-    }
-
-    public TBuilder amount(long amount) {
-        this.transaction.amount = amount;
-        return this.instance();
-    }
-
     public TBuilder sign(String passphrase) {
         this.transaction.sign(passphrase);
         this.transaction.computeId();
-
-        return this.instance();
-    }
-
-    public TBuilder secondSign(String passphrase) {
-        this.transaction.secondSign(passphrase);
-        this.transaction.computeId();
-
         return this.instance();
     }
 
     public TBuilder multiSign(String passphrase, int index) {
         this.transaction.multiSign(passphrase, index);
         this.transaction.computeId();
-
         return this.instance();
+    }
+
+    public TBuilder secondSign(String secondPassphrase) {
+        this.transaction.secondSign(secondPassphrase);
+        this.transaction.computeId();
+        return this.instance();
+    }
+
+    public boolean verify() {
+        return this.transaction.verify();
+    }
+
+    public boolean secondVerify(String secondPublicKey) {
+        return this.transaction.secondVerify(secondPublicKey);
+    }
+
+    public String toJson() {
+        return this.transaction.toJson();
     }
 
     protected abstract AbstractTransaction getTransactionInstance();
